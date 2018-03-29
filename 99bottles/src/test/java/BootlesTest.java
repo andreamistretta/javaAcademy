cb8ddaeb6d1f18b9fc4860be0ce3fdc32ff9e569
andreamistretta <andrea.mistretta04@gmail.com>
2018-03-29 15:15:59 +0200

    create first test

--- 99bottles/src/test/java/BootlesTest.java
+++ 99bottles/src/test/java/BootlesTest.java
@@ -1,9 +1,20 @@
+import com.javaacademy.bottles.Bottles;
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 public class BootlesTest {
 
     @Test
     public void testSong(){
+        final ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        Bottles.writeLyrics(
+                new PrintStream(stream));
 
+        final String actual = new String(stream.toByteArray());
+
+        Assert.assertNotNull(actual);
     }
 }
